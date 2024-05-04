04fb085c9995a66ca681af59fa36f5ba81a82a74
Rajnish sharma <rajnishmig51@gmail.com>
2024-05-04 19:03:37 +0530

    first commit

--- src/app/component/myservices/page.js
+++ src/app/component/myservices/page.js
@@ -3,33 +3,33 @@ import styles from "../../page.module.css";
 
 
 
-export default function myprojects() {
+export default function myservices() {
     return (
         <>
-            {/* <div>
-                <div className={styles.mypro} style={{ width: "10%", color: "white", textAlign: "center", borderRadius: "5px", padding: "10px", marginLeft: "120px", backgroundColor: "#2F2F2F" }}>
+            <div>
+                <div style={{ width: "10%", color: "white", textAlign: "center", borderRadius: "5px", padding: "10px", marginLeft: "120px", backgroundColor: "#2F2F2F" }}>
 
-                    <p >MY WORK</p>
+                    <p>SERVICES</p>
                 </div>
-                <h4 style={{ color: "white", padding: "10px", marginLeft: "110px", }}>R E C E N T &nbsp;&nbsp; P R O J E C T</h4>
+                <h4 style={{ color: "white", padding: "10px", marginLeft: "110px", }}>D E S I G N &nbsp;&nbsp; S E R V I C E S &nbsp;&nbsp; I &nbsp; A M &nbsp;&nbsp; P R O V I D I N G</h4>
 
 
             </div>
 
-            <div className={styles.project}  style={{display:"flex", justifyContent:"space-evenly",   width:"100%", padding:"45px"}}>
-                <div className={styles.projectcard} style={{ backgroundColor:"white", width:"30%",borderRadius:"10px", height:"40vh", padding:"10px"}}>
+            <div style={{display:"flex", justifyContent:"space-evenly",   width:"100%", padding:"45px"}}>
+                <div style={{ backgroundColor:"white", width:"30%",borderRadius:"10px", height:"40vh", padding:"10px"}}>
                     <img style={{width:"100%" , height:"30vh" ,borderRadius:"10px"}} src="https://images.unsplash.com/photo-1517694712202-14dd9538aa97?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"/>
                     <p>Website Design</p>
                 </div>
                 <div style={{ backgroundColor:"white", width:"30%",borderRadius:"10px", height:"40vh", padding:"10px"}}>
                 <img style={{width:"100%" , height:"30vh", borderRadius:"10px"}} src="https://images.unsplash.com/photo-1619410283995-43d9134e7656?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"/>
-                    <p>Backend Restful API</p>
+                    <p>E-Commerce Website</p>
                 </div>
                 <div style={{ backgroundColor:"white", width:"30%",borderRadius:"10px", height:"40vh", padding:"10px"}}>
                 <img style={{width:"100%" , height:"30vh", borderRadius:"10px"}} src="https://images.unsplash.com/photo-1484807352052-23338990c6c6?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"/>
                     <p>Chat Applications</p>
                 </div>
-            </div> */}
+            </div>
         </>
     );
 }
